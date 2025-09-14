a95f56f3fb8b900db8fb22b50c2b7893b2aaf6eb
2077DevWave <2077devwave@gmail.com>
2025-09-14 14:19:24 -0400

    fix compile error :)

--- src/core/Services/ResultService.ts
+++ src/core/Services/ResultService.ts
@@ -1,7 +1,7 @@
 
 import { IResultService } from '../Interfaces/services';
 import { IJsonTestResult, ITestPaths } from '../Interfaces/datastructures';
-import { ICPSTFolderManager, IResultManager } from '../Interfaces/classes';
+import { ICPSTFolderManager} from '../Interfaces/classes';
 import * as path from 'path';
 
 export class ResultService implements IResultService {
